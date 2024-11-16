8e5759fc3856b3452a6dfd80183c46e6b17da213
Phearak <122800934+Samlothphearak@users.noreply.github.com>
2024-11-16 17:03:21 +0700

    update all sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
+    screen: {
+      sm: "340px",
+      md: "540px",
+      lg: "768px",
+      xl: "1180px",
+    },
     extend: {},
+    fontFamily: {
+      Jost: ["Jost", "sans-serif"],
+      Lobster: ["Lobster", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        default: "12px",
+        md: "32px",
+      }
+    }
   },
   plugins: [],
 }
-
